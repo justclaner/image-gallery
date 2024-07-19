2fcf54f6be62f7e7253913f3d3564648c9ebd9ca
Justin Zou <justclaner@gmail.com>
2024-07-19 17:25:56 -0400

    added feature to drag images around

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -6,7 +6,7 @@ function Gallery(props) {
 
 
 
-const initialUrls = ["https://pics.craiyon.com/2023-11-04/445a2f0ac518410fac24c2ae3980787b.webp","https://pbs.twimg.com/media/FO35aoHXMAIceaL.jpg"];
+const initialUrls = ["https://pics.craiyon.com/2023-11-04/445a2f0ac518410fac24c2ae3980787b.webp","https://pbs.twimg.com/media/FO35aoHXMAIceaL.jpg","https://upload.wikimedia.org/wikipedia/commons/thumb/d/d1/Mount_Everest_as_seen_from_Drukair2_PLW_edit.jpg/1200px-Mount_Everest_as_seen_from_Drukair2_PLW_edit.jpg"];
 const initialImages = initialUrls.map((url,index)=><Image img={url} name="Sample File Name" desc="Sample Image Description. Click to edit!" key={index} id={index}></Image>);
 
 
--- src/components/ImageCard.jsx
+++ src/components/ImageCard.jsx
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types'
-import {useState, useContext} from 'react'
+import {useState, useContext, useRef} from 'react'
 import {ImageContext} from '../App.jsx'
-function Image({img, name = "Random Name", desc = "Random Description", id}) {
+export function Image({img, name = "Random Name", desc = "Random Description", id}) {
 const imageContext = useContext(ImageContext);
 
 
@@ -12,8 +12,28 @@ return(
         imageContext.setImageSelected(id);
         console.log(imageContext.imageSelected);
     }}
-
+    draggable="true"
     style={imageContext.imageSelected == id ? {border: "5px solid red"} : {border: "5px solid black"}}
+    onDrag={()=>{
+        let index = -1;
+        for (let i = 0; i < imageContext.imageList.length; i++) {
+            if (imageContext.imageList[i].props.id == id) {index = i; console.log("index1 " + index); break;}
+        }
+        imageContext.setMoveIds([index,imageContext.moveIds[1]]);
+        imageContext.setMoveImage({img,name,desc,id});
+        imageContext.setDragEnded(false);
+    }}
+    onDragOver={(e)=>{e.preventDefault();}}
+    onDragEnter={(e)=>{e.preventDefault();}}
+    onDrop={()=>{
+        imageContext.setDragEnded(true)
+        let index = -1;
+        for (let i = 0; i < imageContext.imageList.length; i++) {
+            if (imageContext.imageList[i].props.id == id) {index = i; console.log("index2 " + index); break;}
+        }
+        imageContext.setMoveIds([imageContext.moveIds[0],index]);
+    }}
+    onDragEnd={()=>{console.log("ended"); }}
         >
     <img src={img} id={id} draggable="false" style={imageContext.imageSelected == id ? {borderBottom: "3px solid red"} : {borderBottom: "3px solid black"}}/> 
     <p className="image-name">{name.length > 30 ? name.slice(0,25) + "..." + name.slice(-4) : name}</p>
