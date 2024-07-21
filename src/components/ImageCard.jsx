d4bf77b7f5715c8ebd5fc4f3b64a7f23593d572a
Justin Zou <justclaner@gmail.com>
2024-07-21 18:50:37 -0400

    Update default image name and description

--- src/components/ImageCard.jsx
+++ src/components/ImageCard.jsx
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types'
 import {useContext} from 'react'
 import {ImageContext} from '../App.jsx'
-export function Image({img, name = "Random Name", desc = "Random Description", id}) {
+export function Image({img, name = "Sample Name", desc = "Sample Description.\nClick to edit!", id}) {
 const imageContext = useContext(ImageContext);
 
 return(
@@ -18,22 +18,27 @@ return(
     <img src={img} id={id} style={imageContext.imageSelected == id ? {borderBottom: "3px solid #ee6c4d"} : {borderBottom: "3px solid black"}}
     draggable="true"
     onDrag={()=>{
+        //search for image index in the list to allow correct switching of images
         let index = -1;
         for (let i = 0; i < imageContext.imageList.length; i++) {
             if (imageContext.imageList[i].props.id == id) {index = i; break;}
         }
+        //set selected image index
         imageContext.setMoveIds([index,imageContext.moveIds[1]]);
         imageContext.setMoveImage({img,name,desc,id});
         imageContext.setDragEnded(false);
     }}
+    //allow dropping without a drop target
     onDragOver={(e)=>{e.preventDefault();}}
     onDragEnter={(e)=>{e.preventDefault();}}
     onDrop={()=>{
         imageContext.setDragEnded(true)
+        //search for image index in the list to allow correct switching of images
         let index = -1;
         for (let i = 0; i < imageContext.imageList.length; i++) {
             if (imageContext.imageList[i].props.id == id) {index = i; break;}
         }
+        //set target image index
         imageContext.setMoveIds([imageContext.moveIds[0],index]);
     }}
     /> <br />
